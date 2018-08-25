0a35f52458815d480100d8a7b8ebd9bccf00ae6f
Laurence Rowe <lrowe@netflix.com>
2018-08-25 11:54:38 -0700

    larger test data

--- test/test-cache.js
+++ test/test-cache.js
@@ -4,26 +4,40 @@ const { expect } = require("chai");
 const { CacheDataSource } = require("../src/cache");
 const tmp = require("tmp");
 
-const basicLolomo = {
-  jsonGraph: {
-    lolomo: { $type: "ref", value: ["lolomos", "ABC"] },
-    lolomos: {
-      ABC: {
-        "0": { $type: "ref", value: ["lists", "ABC0"] }
-      }
-    },
-    lists: {
-      ABC0: {
-        "0": { $type: "ref", value: ["videos", 123] }
-      }
-    },
-    videos: {
-      "123": {
-        title: "Stranger Things"
-      }
-    }
+function range(start: number, stop?: number): number[] {
+  if (stop === undefined) {
+    stop = start;
+    start = 0;
   }
-};
+  return Array(stop - start)
+    .fill(start)
+    .map((x, i) => x + i);
+}
+
+function makeLolomo(rows: number = 1, cols: number = 1, id: string = "ABC") {
+  const lolomo = { $type: "ref", value: ["lolomos", id] };
+  const lolomos = {
+    [id]: range(rows)
+      .map(i => ({ $type: "ref", value: ["lists", `${id}${i}`] }))
+      .reduce((acc, v, i) => ({ ...acc, [i]: v }), {})
+  };
+  const lists = range(rows)
+    .map(i =>
+      range(cols)
+        .map(j => ({ $type: "ref", value: ["videos", i * cols + j] }))
+        .reduce((acc, v, j) => ({ ...acc, [j]: v }), {})
+    )
+    .reduce((acc, v, i) => ({ ...acc, [`${id}${i}`]: v }), {});
+  const videos = range(rows * cols)
+    .map(x => ({ title: `title ${Math.trunc(x / cols)} ${x % cols}` }))
+    .reduce((acc, v, x) => ({ ...acc, [x]: v }), {});
+  const jsonGraph = { lolomo, lolomos, lists, videos };
+  return { jsonGraph };
+}
+
+const ROWS = 2;
+const COLS = 2;
+const basicLolomo = makeLolomo(ROWS, COLS);
 
 describe("CacheDataSource", function() {
   let tmpdir;
@@ -44,7 +58,7 @@ describe("CacheDataSource", function() {
     );
     ds._setCache(basicLolomo);
     const keys = Array.from(ds.keys());
-    expect(keys.length).to.equal(4);
+    expect(keys.length).to.equal(1 + ROWS + ROWS * COLS + ROWS * COLS);
   });
 
   it("gets data", function(done) {
@@ -54,7 +68,7 @@ describe("CacheDataSource", function() {
       { name: "mydb", create: true }
     );
     ds._setCache(basicLolomo);
-    ds.get([["lolomo", 0, 0, "title"]]).subscribe(
+    ds.get([["lolomo", { length: ROWS }, { length: COLS }, "title"]]).subscribe(
       result => {
         expect(result.jsonGraph).to.deep.equal(basicLolomo.jsonGraph);
       },
